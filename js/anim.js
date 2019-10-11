f5033352599f6ec6b8ce94505b6ffbce8ebaaccd
Daniel <dan.amaralsilva@gmail.com>
2019-10-10 22:00:34 -0300

    atualizada animação das logos
    movida a página para a página inicial

--- js/anim.js
+++ js/anim.js
@@ -1,2 +1,18 @@
 // fix that
-import anime from 'js/animejs/anime.es.js';
+var elem = '.svg-icon path';
+
+var timeline = anime.timeline({
+        easing: 'linear',
+        duration: 1500,
+        loop: false
+    }
+);
+
+timeline.add({
+    targets: elem,
+    strokeDashoffset: [anime.setDashoffset, 0]
+})
+.add({
+    targets: elem,
+    fillOpacity: ['0', '1'],
+}, 1000);
