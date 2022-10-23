8eb645cb8ae7a6ea51ca7fdb21c16207b28af840
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2022-10-23 11:48:46 -0300

    criada a tela de contato

--- src/Components/Contato.jsx
+++ src/Components/Contato.jsx
@@ -1,14 +1,27 @@
+import { ReactComponent as Github } from "../assets/github.svg";
+import { ReactComponent as Linkedin } from "../assets/linkedin.svg";
 const Contato = () => {
   return (
-    <section id="contato">
+    <section id="contato" className="contato">
       <h2 className={"title decor"}>
         <code>04.</code> Contato
       </h2>
-      <p>
+      <p className="contato__text">
         Atualmente estou disponível para oportunidades de trabalho. Na verdade,
         mesmo se não for uma oportunidade de trabalho, mas um papo descontraído
         à cerca de front end, pode me chamar.
       </p>
+      <a className="contato__email" href="mailto:dan.amaralsilva@gmail.com">
+        <code>Email</code>
+      </a>
+      <div className="contato__externals">
+        <a href="https://github.com/viralatron">
+          <Github />
+        </a>
+        <a href="https://www.linkedin.com/in/dan-amarals/">
+          <Linkedin />
+        </a>
+      </div>
     </section>
   );
 };
