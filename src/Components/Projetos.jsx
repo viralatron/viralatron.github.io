91cfa66150e317a4115df3a73f74e2d67acb1201
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2022-12-21 18:05:47 -0300

    minor fix at fetch urls

--- src/Components/Projetos.jsx
+++ src/Components/Projetos.jsx
@@ -8,7 +8,7 @@ const Projetos = () => {
 
   const fetchData = async () => {
     const getData = await fetch(
-      `https://gist.githubusercontent.com/viralatron/c638b4f83ee0669c0310fe8a32f960d5/raw/fe6157f1b23d5a1690505f7063b16c7bb42eb98a/projects.json`
+      `https://gist.githubusercontent.com/viralatron/c638b4f83ee0669c0310fe8a32f960d5/raw/projects.json`
     );
     const response = await getData.json();
     setProjetos(response.projetos);
