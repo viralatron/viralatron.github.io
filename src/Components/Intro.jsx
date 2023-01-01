10440f2fad5fc561d352933d4f0d860b15d5211c
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2023-01-01 17:11:08 -0300

    partially revert text changes

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -5,13 +5,12 @@ const Intro = () => {
         <code>oi, eu sou</code>
         Daniel Amaral e Silva
         <small>e sou dev Front End</small>
-        {/* <small>e faço páginas web</small> */}
       </h1>
-      <h2>
+      <p>
         Sou um desenvolvedor de software com quase dez anos de experiência em
         HTML, CSS e PHP/Joomla. Atualmente busco recolocação como front end
         especializado em React.
-      </h2>
+      </p>
     </header>
   );
 };
