ba0b6cbf064361b4e8b1785b123095f0797039e3
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2023-01-01 15:43:41 -0300

    text changes, color changes

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -4,13 +4,14 @@ const Intro = () => {
       <h1>
         <code>oi, eu sou</code>
         Daniel Amaral e Silva
-        <small>e faço páginas web</small>
+        <small>e sou dev Front End</small>
+        {/* <small>e faço páginas web</small> */}
       </h1>
-      <p>
-        sou um desenvolvedor de software com quase dez anos de experiência em
-        HTML, CSS e PHP/Joomla. atualmente busco recolocação como front end
+      <h2>
+        Sou um desenvolvedor de software com quase dez anos de experiência em
+        HTML, CSS e PHP/Joomla. Atualmente busco recolocação como front end
         especializado em React.
-      </p>
+      </h2>
     </header>
   );
 };
