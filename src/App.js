1f8f2c948a69a214926648872b615139ac8a9f54
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2023-03-29 09:38:55 -0300

    diagonal implementation

--- src/App.js
+++ src/App.js
@@ -5,15 +5,44 @@ import Projetos from "./Components/Projetos";
 import Contato from "./Components/Contato";
 import Menu from "./Components/Menu";
 import "./Styling/App.scss";
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const container = document.querySelector(".container");
+      container.style.transform = `
+                translate(${-Math.floor(window.scrollY) * 0.555 + 80}px,
+                ${-Math.floor(window.scrollY)}px)
+                rotateX(50deg)
+                rotateZ(-20deg)
+            `;
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {
+        const container = document.querySelector(".container");
+        container.style.transform = `
+                    translate(${-Math.floor(window.scrollY) * 0.555 + 80}px,
+                    ${-Math.floor(window.scrollY)}px)
+                    rotateX(50deg)
+                rotateZ(-20deg)
+                `;
+      });
+    };
+  }, []);
   return (
     <main className="App">
       <Menu />
-      <Intro />
-      <Sobre />
-      <ExpProfissional />
-      <Projetos />
-      <Contato />
+      <div className="container">
+        <div className="sections">
+          <Intro />
+          <Sobre />
+          <ExpProfissional />
+          <Projetos />
+          <Contato />
+        </div>
+      </div>
+      <div className="space"></div>
     </main>
   );
 }
