8fbd5d7c9bb139395cc276eb167e34ce655d7a76
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2023-08-10 17:58:27 -0300

    chore: remoção de código não utilizado

--- src/Components/SectionTitle.jsx
+++ src/Components/SectionTitle.jsx
@@ -1,20 +1,8 @@
-import { useEffect, useRef, useState } from "react";
-import { findDOMNode } from "react-dom";
 import "../Styling/Section.scss";
 
 const SectionTitle = ({ itemNr, title }) => {
-  const titleRef = useRef(null);
-  const [width, setWidth] = useState(null);
-
-  useEffect(() => {
-    const titleWidth = findDOMNode(titleRef.current).getElementsByClassName(
-      "title__text"
-    )[0].offsetWidth;
-    const divWidth = findDOMNode(titleRef.current).offsetWidth;
-    setWidth(divWidth - (titleWidth + 52));
-  }, [titleRef]);
   return (
-    <div className="title" ref={titleRef}>
+    <div className="title">
       <h2 className={"title__text"}>
         <code>{itemNr}.</code> {title}
       </h2>
